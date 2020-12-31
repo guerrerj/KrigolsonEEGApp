5cbefea7b1faa993fe80a0670c7db43bbe23ae0d
Xin <xin@Xins-MacBook.hitronhub.home>
2020-12-31 14:16:00 -0500

    bci route updated before display a bar chart for ave

--- EEGApp/src/app/bci/bci.component.ts
+++ EEGApp/src/app/bci/bci.component.ts
@@ -1,15 +1,182 @@
-import { Component, OnInit } from '@angular/core';
+import { DataService } from './../shared/dataService';
+import { backgroundColors, borderColors,  channelLabels, FreqSpectraChartOptions, getSettings, ISettings } from './../shared/bciChartOptions';
+import { Component, Input, AfterViewInit, AfterViewChecked } from '@angular/core';
+import { OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { channelNames, EEGSample } from 'muse-js';
+import { takeUntil } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
+import { Chart } from 'chart.js';
+
+
+import {spectraDataSet } from '../shared/chartOptions';
+import {
+  bandpassFilter,
+  epoch,
+  fft,
+  sliceFFT
+} from '@neurosity/pipes';
+
+// If you have inner observable use mergemap to allow you to  subscribe to directly to it after applying map operation
+
+const chartStyles = {
+  wrapperStyle: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    padding: '20px'
+  }
+};
+
 
 @Component({
   selector: 'app-bci',
   templateUrl: './bci.component.html',
   styleUrls: ['./bci.component.less']
 })
-export class BciComponent implements OnInit {
 
-  constructor() { }
+
+export class BciComponent implements OnInit, OnDestroy, AfterViewInit, AfterViewChecked {
+
+  @Input() data: Observable<EEGSample>;
+
+  settings: ISettings;
+
+  readonly destroy = new Subject<void>();
+  readonly channelNames = channelNames;
+
+  chart: Chart;
+
+  constructor(private incomingData: DataService) {}
 
   ngOnInit(): void {
+    // Get settings for the charts
+    this.settings = getSettings();
+    this.settings.name = 'Frequency Spectrum';
+
+    // Get the chart options such as adding dummy data and configurations
+    const canvas = document.getElementById('freqChart') as HTMLCanvasElement;
+    const dataSets = [];
+
+    Array(this.settings.nChannels).fill(0).map((ch, i) => {
+          const temp =  Object.assign({}, spectraDataSet);
+          temp.backgroundColor = backgroundColors[i];
+          temp.borderColor = borderColors[i];
+          temp.label = channelLabels[i];
+          temp.data  = Array(this.settings.maxFreq).fill(0);
+          dataSets.push(temp);
+        });
+
+    // Instantiate the chart with the options
+    this.chart = new Chart(canvas, {
+          type: 'line',
+          data: {
+            datasets: [dataSets[0], dataSets[1], dataSets[2], dataSets[3]],
+            labels: [],
+        },
+        options: FreqSpectraChartOptions
+      });
+
+
+    // const canvas = document.getElementById('channelAve') as HTMLCanvasElement;
+
+
   }
 
+  ngAfterViewInit(): void {
+  }
+
+  ngAfterViewChecked(): void {
+    // Check if the data is available to start processing it
+
+    if (this.incomingData.data != null && this.data == null) {
+      this.incomingData.data.pipe(samples => this.data = samples);
+      this.data.pipe(
+        takeUntil(this.destroy),
+        bandpassFilter({
+          cutoffFrequencies: [this.settings.cutOffLow, this.settings.cutOffHigh],
+          nbChannels: this.settings.nChannels }),
+        epoch({
+          duration: this.settings.duration,
+          interval: this.settings.interval,
+          samplingRate: this.settings.srate
+        }),
+        fft({bins: this.settings.bins }),
+        sliceFFT([this.settings.sliceFFTLow, this.settings.sliceFFTHigh]),
+        catchError(async (err) => console.log(err))
+      )
+        // Finally after data has been processed, subscribe the component to it
+        .subscribe(data => {
+          this.addData(data);
+          this.addChannelData(data);
+         
+        });
+      }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next();
+  }
+
+  // Add the processed data to the charts
+  addData(spectraData: any): void {
+    for (let i = 0; i < this.settings.nChannels; i++) {
+      // remove old data by setting length to 0
+      this.chart.data.datasets[i].data.length = 0;
+      spectraData.psd[i].forEach((val: number) => this.chart.data.datasets[i].data.push(val));
+      // add labels if they have not been added up until 31 labels
+      if (this.chart.data.labels.length < this.settings.maxFreq - 1)
+      {
+        this.chart.data.labels.length = 0;
+        // get the freq labels that are below the maxFreq required
+        const freqs  = spectraData.freqs.filter((x: number) => x < this.settings.maxFreq);
+        freqs.forEach((val: number) => this.chart.data.labels.push(val));
+      }
+    }
+    this.chart.update();
+  }
+
+  channelData=[[],[],[],[],[]];
+  Tp9Freq8= Array(10).fill(0);
+  Tp9Freq8Ave= Array(10).fill(0);
+  addChannelData (spectraData: any): void {
+  	console.log('spectraData: ', spectraData);
+  	var ave,std;
+  	for (let i = 0; i < this.settings.nChannels; i++){
+  		if(i==0){   // i=0 freqency at 8 hz
+  			// console.log('psd[',i,'][0]:',spectraData.psd[i][0]);	
+		 	this.Tp9Freq8.shift();
+		 	this.Tp9Freq8.push(spectraData.psd[i][0]);
+			this.Tp9Freq8Ave.shift();
+			this.Tp9Freq8Ave.push(this.average(this.Tp9Freq8));
+			std=this.standardDeviation(this.Tp9Freq8Ave);
+		}	  		
+  	}
+  	//display the average as bar chart 
+  	console.log('TP9Freq8:  ',this.Tp9Freq8);
+  	console.log('Tp9Freq8Ave:  ',this.Tp9Freq8Ave);
+  	console.log('std: ',std);
+  }
+
+
+
+
+
+
+    // Used to calculate average of values in data
+  average(data: Array<number>): number {
+      const sum = data.reduce((sumTemp: number, value: number) => sumTemp + value, 0);
+      return sum / data.length;
+  }
+
+  // Standard deviation of values in values
+  standardDeviation(values: Array<number>): string{
+    const avg = this.average(values);
+    const squareDiffs = values.map((value: number) => Math.pow((value - avg), 2));
+    const avgSquareDiff = this.average(squareDiffs);
+    const stdDev = Math.sqrt(avgSquareDiff).toFixed(0);
+    return stdDev;
+  }
+
+
 }
+
---
+++ EEGApp/src/app/shared/bciChartOptions.ts
@@ -0,0 +1,186 @@
+import { ChartOptions } from 'chart.js';
+
+/* This file contains the settings and options
+used by different chart providers (i.e chart.js and smoothie.js)
+for each application
+*/
+export const bandLabels = ['Alpha', 'Beta', 'Delta', 'Gamma', 'Theta'];
+export const orderedBandLabels = ['Delta', 'Theta', 'Alpha', 'Beta'];
+export const channelLabels = ['TP9', 'AF7', 'AF8', 'TP10'];
+export const orderedLabels = ['AF7', 'AF8', 'TP9', 'TP10'];
+
+
+export const backgroundColors: string[] = [
+  'rgba(255, 99, 132, 0.2)',
+  'rgba(54, 162, 235, 0.2)',
+  'rgba(255, 206, 26, 0.2)',
+  'rgba(75, 92, 192, 0.2)',
+  'rgba(153, 102, 255, 0.2)',
+];
+
+export const borderColors: string[] = ['rgba(255, 99, 132, 1)',
+'rgba(54, 162, 235, 1)',
+'rgba(255, 206, 26, 1)',
+'rgba(75, 92, 192, 1)',
+'rgba(153, 102, 255, 1)',
+];
+
+interface ISpectraDataSet {
+  label: string;
+  data: number[];
+  backgroundColor: string;
+  borderColor: string;
+  borderWidth: number;
+  fill: any;
+  lineTension: number;
+}
+export const spectraDataSet: ISpectraDataSet = {
+  data: [],
+  backgroundColor: '' ,
+  borderColor: '',
+  borderWidth: 1,
+  fill: false,
+  label: '',
+  lineTension: 0.4
+};
+
+interface IBandsDataSet {
+  label: string[];
+  data: number[];
+  backgroundColor: string;
+  borderColor: string;
+  borderWidth: number;
+  fill: any;
+  lineTension: number;
+}
+export const bandsDataSet: IBandsDataSet = {
+  data: [],
+  backgroundColor: '' ,
+  borderColor: '',
+  borderWidth: 1,
+  fill: false,
+  label: [],
+  lineTension: 0.4
+};
+
+export const spectraLabels = []
+
+export const FreqSpectraChartOptions: Partial<ChartOptions> = {
+  events: ['mousemove', 'mouseout', 'click', 'touchstart', 'touchmove'],
+  tooltips: {
+    enabled: true,
+    intersect: true,
+    mode: 'point',
+    callbacks: {
+      label(tooltipItem: any, data) : string {
+          let label = data.datasets[tooltipItem.datasetIndex].label || '';
+
+          if (label) {
+              label += ': ';
+          }
+          label += Math.round(tooltipItem.yLabel * 100) / 100;
+          return label;
+      }
+    }
+  },
+  hover: {
+    animationDuration: 0
+  },
+  responsiveAnimationDuration: 0,
+  title: {
+    display: true,
+    text: 'Frequency Spectra per Electrode'
+  },
+  scales: {
+    yAxes: [{
+      scaleLabel: {
+      display: true,
+      labelString: 'Power (uV)'
+    }}],
+    xAxes: [{
+      scaleLabel: {
+        display: true,
+        labelString: 'Frequency (Hz)'
+      }
+    }]
+ }
+};
+
+
+export const FreqBandsChartOptions: Partial<ChartOptions> = {
+  title: {
+    display: true,
+    text: 'Frequency Bands per Electrode'
+  },
+  responsiveAnimationDuration: 0,
+  scales: {
+      yAxes: [{
+        scaleLabel: {
+        display: true,
+        labelString: 'Power (uV)'
+      }}],
+      xAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'Frequency Bands'
+        }
+      }]
+  }
+};
+
+export const FreqExperChartOptions: Partial<ChartOptions> = {
+   title: {
+     display: true,
+     text: 'Power'
+   },
+   responsiveAnimationDuration: 0,
+   scales: {
+     yAxes: [{
+       scaleLabel: {
+         display: true,
+         labelString: 'Power (uV)'
+       }
+     }],
+     xAxes: [{
+       scaleLabel: {
+         display: true,
+         labelString: 'Time (sample number)'
+       }
+     }]
+   },
+   legend: {
+     display: false
+   }
+};
+
+export interface ISettings {
+  cutOffLow: number;
+  cutOffHigh: number;
+  interval: number;
+  bins: number;
+  duration: number;
+  srate: number;
+  name: string;
+  secondsToSave: number;
+  nChannels: number;
+  sliceFFTLow?: number;
+  sliceFFTHigh?: number;
+  maxFreq: number;
+}
+
+export function getSettings(): ISettings {
+  return {
+    cutOffLow: 8,
+    cutOffHigh: 12,
+    interval: 200,
+    bins: 256,
+    duration: 1024,
+    srate: 256,
+    name: '',
+    secondsToSave: 10,
+    nChannels: 4,
+    sliceFFTLow: 8,
+    sliceFFTHigh: 12,
+    maxFreq : 13
+  };
+}
